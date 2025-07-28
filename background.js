e7a32213b7bf186e44dea64eb04005d18e83d3e7
Radja Lomas <radjal@free.fr>
2025-07-28 20:28:33 +0200

    ChatGPT generated code for inline version of Jisho lookup

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "jishoInlineLookup",
-    title: "Look up on Jisho.org (Iframe)",
+    title: "Look up on Jisho.org",
     contexts: ["selection"]
   });
 });
@@ -12,5 +12,16 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       type: "JISHO_LOOKUP",
       text: info.selectionText
     });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "FETCH_JISHO") {
+    const url = `https://jisho.org/api/v1/search/words?keyword=${encodeURIComponent(request.query)}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => sendResponse({ success: true, data }))
+      .catch(error => sendResponse({ success: false, error: error.message }));
+    return true;
   }
 });
